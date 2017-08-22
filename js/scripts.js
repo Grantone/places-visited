1ac296ffa6f53319de242f6239622b8213793bce
gm and sb <gm@>
2017-08-22 14:14:14 +0300

    Add the variables for taking form inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Business logic
 
-function PlacesVisited() {
+function PlacesVisited(location,landmark,year,notes) {
   this.location=location;
   this.landmark=landmark;
   this.year = year;
@@ -10,3 +10,17 @@ function PlacesVisited() {
 
 
 //user interface
+
+$(document).ready(function() {
+    $("form#places-visited").submit(function(event) {
+      event.preventDefault();
+
+      var inputtedFirstLocation = $("input#firstLocation").val();
+      var inputtedLandMark = $("input#landmark").val();
+      var inputtedYear = $("input#timeOfYear").val();
+      var inputtedNotes= $("input#destinationNotes").val();
+
+    
+
+    });
+  });
