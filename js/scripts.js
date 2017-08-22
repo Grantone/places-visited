42bbbdbdf6f7441680269ab9a33732057379bc5d
gm and sb <gm@>
2017-08-22 12:24:38 +0300

    Add the constructor for the various properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
+//Business logic
+
 function PlacesVisited() {
   this.location=location;
   this.landmark=landmark;
-  this.year=year;
+  this.year = year;
   this.notes=notes;
 
 }
+
+
+//user interface
